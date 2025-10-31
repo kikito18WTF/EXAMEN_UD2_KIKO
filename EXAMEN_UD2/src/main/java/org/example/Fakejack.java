b2cc59481ba2df47a05487a51fc2af81f97b3340
A615\1DAW <kikoperezcanto10@gmail.com>
2025-10-31 15:41:41 +0100

    Examen

---
+++ EXAMEN_UD2/src/main/java/org/example/Fakejack.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class Fakejack {
+
+    static void main() {
+
+    }
+
+}
---
+++ EXAMEN_UD2/src/main/java/org/example/Vocales.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class Vocales {
+
+    static void main() {
+
+    }
+
+}
